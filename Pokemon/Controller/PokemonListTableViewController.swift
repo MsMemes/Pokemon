cde6440e0ac130e86ade1531d942ad6c73b66d1a
Julia Jimenez Herrera <maggie.jh99@gmail.com>
2022-05-04 09:51:28 -0500

    Final version

--- Pokemon/Controller/PokemonListTableViewController.swift
+++ Pokemon/Controller/PokemonListTableViewController.swift
@@ -25,19 +25,6 @@ class PokemonListTableViewController: UITableViewController {
         
     }
     
-    func readJSON(forName name: String) -> Data? {
-        do {
-            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
-                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
-                return jsonData
-            }
-        } catch {
-            print(error)
-        }
-        
-        return nil
-    }
-    
     private func parse(jsonData: Data) {
         do {
             let decodedData = try JSONDecoder().decode(PokemonModel.self, from: jsonData)
